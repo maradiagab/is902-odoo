739d9067925a65135176a22956f60ffcabf822a4
David Maradiaga <dmaradiaga08@gmail.com>
2020-07-15 15:37:20 -0600

    [ADD] One to many

--- openacademy/models/course.py
+++ openacademy/models/course.py
@@ -9,6 +9,8 @@ class Course(models.Model):
     description = fields.Text()
     responsible_id = fields.Many2one('res.users',
                                      ondelete='set null', string="Responsible", index=True)
+    session_ids = fields.One2many(
+        'openacademy.session', 'course_id', string="Sessions")
 
 
 class Session(models.Model):
@@ -23,3 +25,4 @@ class Session(models.Model):
     instructor_id = fields.Many2one('res.partner', string="Instructor")
     course_id = fields.Many2one('openacademy.course',
                                 ondelete='cascade', string="Course", required=True)
+    attendee_ids = fields.Many2many('res.partner', string="Attendees")
