4ba1820595700a1c54302af72859b5a931d0c5f4
David Maradiaga <dmaradiaga08@gmail.com>
2020-07-15 09:00:10 -0600

    [ADD] course.py

--- openacademy/models.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import api, fields, models
-
-# class openacademy(models.Model):
-#     _name = 'openacademy.openacademy'
-
-#     name = fields.Char()
---
+++ openacademy/models/course.py
@@ -0,0 +1,9 @@
+from odoo import api, fields, models
+
+
+class Course(models.Model):
+    _name = 'openacademy.course'
+    _description = "OpenAcademy Courses"
+
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
