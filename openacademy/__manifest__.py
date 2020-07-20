ac59f150b1e31157fb4a845abfea4f77a9718aa7
David Maradiaga <dmaradiaga08@gmail.com>
2020-07-20 00:42:48 -0600

    [ADD] reporting

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -21,7 +21,8 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'board'],
+
 
     # always loaded
     'data': [
@@ -30,6 +31,8 @@
         'views/templates.xml',
         "views/openacademy.xml",
         'views/partner.xml',
+        'views/session_board.xml',
+        'reports.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
